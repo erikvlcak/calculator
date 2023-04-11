fa0d3cc6cb074fffa3b7941eef4cd8e757d58c9e
erikvlcak <erikvlcak1@gmail.com>
2023-04-12 00:33:00 +0200

    Add mouse events for buttons

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let input = document.querySelector('.display .input');
+let result = document.querySelector('.display .result');
+let buttons = document.querySelector('.buttons');
+let num1 = [];
+let num2 = [];
+let operator;
+
+buttons.addEventListener('mousedown', (e) => {
+
+    e.target.style.boxShadow = '0px 0px';
+})
+
+buttons.addEventListener('mouseup', (e) => {
+
+
+
+    if (e.target.className != 'buttons') {
+        num1 = e.target.textContent;
+        e.target.style.boxShadow = '3px 3px';
+        input.textContent = num1;
+    }
+    console.log(e.target)
+})
