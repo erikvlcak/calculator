bfd8ec56b23718aa5f69668ea8c92efd21c9396a
erikvlcak <erikvlcak1@gmail.com>
2023-04-13 22:34:19 +0200

    Add decimal point functionality, fix error with 0
    -user can add only one decimal per number
    -fix problem with multiple 0 inputs in a row before decimal point

--- script.js
+++ script.js
@@ -3,10 +3,14 @@
 let input = document.querySelector('.display .input');
 let result = document.querySelector('.display .result');
 let buttons = document.querySelector('.buttons');
-let num1 = [];
+let num1 = [0];
 let num2 = [];
 let res = [];
 let operator = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+    result.textContent = num1[0];
+})
 
 buttons.addEventListener('mousedown', (e) => {
     //click effect
@@ -23,8 +27,15 @@ buttons.addEventListener('mouseup', (e) => {
     //if operator already selected, add as num2, if not, add as num1
     if ((e.target.classList.contains('number'))) {
         if (operator != '') {
-            num2.push(e.target.textContent);
-            input.textContent = `${num1.join('')} ${operator} ${num2.join('')}`;
+            if ((!(num2[0] == 0)) || ((num2[1] == '.'))) {
+                num2.push(e.target.textContent);
+                result.textContent = `${num2.join('')}`;
+                input.textContent = `${num1.join('')} ${operator} ${num2.join('')}`;
+            }
+        } else if ((num1[0] == 0) && (!(num1[1] == '.'))) {
+            num1.pop();
+            num1.push(e.target.textContent);
+            result.textContent = `${num1.join('')} ${operator} ${num2.join('')}`;
         } else {
             num1.push(e.target.textContent);
             result.textContent = `${num1.join('')} ${operator} ${num2.join('')}`;
@@ -39,14 +50,14 @@ buttons.addEventListener('mouseup', (e) => {
 
     //display result after pressing = if num2 has benn added, hence all 3 variables are known, display whole equation as input value
     if ((e.target.classList.contains('=') && (num2.length != 0))) {
-        result.textContent = evaluate(num1, num2, operator);
+        result.textContent = evaluate(num1, num2, operator).toFixed(1);
         res.push(result.textContent);
         input.textContent = `${num1.join('')} ${operator} ${num2.join('')} =`;
     }
 
     //display result of equation after a new operator has been selected if all 3 variables are known and = is not the operator
     if ((e.target.classList.contains('operator')) && (num2.length != 0) && (!(e.target.classList.contains('=')))) {
-        result.textContent = evaluate(num1, num2, operator);
+        result.textContent = evaluate(num1, num2, operator).toFixed(1);
         operator = e.target.textContent;
         num1.length = 0;
         num2.length = 0;
@@ -54,21 +65,39 @@ buttons.addEventListener('mouseup', (e) => {
         input.textContent = `${result.textContent} ${operator}`;
     }
 
-    console.log(`num1 je: ${num1}`);
-    console.log(`operator je: ${operator}`);
-    console.log(`num2 je: ${num2}`);
+
 
     //reset all 3 variables, input and result values
     if (e.target.classList.contains('C')) {
         num1.length = 0;
+        num1.push(0);
         num2.length = 0;
         res.length = 0;
         operator = '';
         input.textContent = '';
         result.textContent = 0;
     }
-})
 
+    //add decimal point to either num1 or num2 if it is not already present in either number
+    if ((e.target.classList.contains('.'))) {
+        if (operator != '') {
+            if (!(num2.includes(e.target.textContent))) {
+                num2.push(e.target.textContent);
+                result.textContent = `${num2.join('')}`;
+                input.textContent = `${num1.join('')} ${operator} ${num2.join('')}`;
+            }
+        } else {
+            if (!(num1.includes(e.target.textContent))) {
+                num1.push(e.target.textContent);
+                result.textContent = `${num1.join('')} ${operator} ${num2.join('')}`;
+            }
+        }
+    }
+
+    console.log(`num1 je: ${num1}`);
+    console.log(`operator je: ${operator}`);
+    console.log(`num2 je: ${num2}`);
+})
 
 
 
